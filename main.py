8ea486a2c5a0850a44ce7c415e2fe734de580cd1
David & Kim Brown <davidkimb1980@gmail.com>
2023-08-19 06:09:30 +0000

    added to excel file

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import advertools as adv
 from advertools import crawl
-import pd
+import pandas as pad
 urls = [
     "https://friv.com/",
     "https://lottie.com/",
     "https://minerva.com/",
-    "https://sockshop.co.uk/",
     "https://nottinghamforest.co.uk/",
     "https://seekingalpha.com/",
     "https://gem-a.com/",
-    "https://microsoft.com/",
+    "https://www.microsoft.com/en-gb/",
     "https://apple.com/",
     "https://west-quay.co.uk/",
     "https://johnlewis.com/",
@@ -18,3 +17,5 @@ urls = [
 
 crawl(urls, 'output.jl', follow_links=True)
 
+crawl_df = pad.read_json('output.jl', lines=True)
+crawl_df.to_excel('output.xlsx', index=False)
